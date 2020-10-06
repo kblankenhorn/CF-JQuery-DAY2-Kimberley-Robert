422b3939addf317c21a0ffb15d797cc7054202cc
Kimberley Blankenhorn <kvandlen@yahoo.com>
2020-10-06 14:42:58 +0200

    advanced unfinished

--- intermediate/familyAlbum.js
+++ intermediate/familyAlbum.js
@@ -8,81 +8,66 @@ var jsonData = JSON.parse(images);
 //after creating the divs with the images, we select the id of the created image, and append the text as a span to it
 for(var i=0;i<jsonData.length;i++)
 {
-    $(".container").append(`<div id="${i}" class="img-container"><img id="img${i}"src='${jsonData[i].image}'></div>`);
-   // $(`#${i}`).append(`<br><span>Relation: ${jsonData[i].relation}</span>`);
-  //  $(`#${i}`).append(`<br><span>Name: ${jsonData[i].name}</span>`);
+    $(".container").append(`<div serri="${i}" class="img-container"><img src='${jsonData[i].image}'><p></p></div>`);
+
 }
 
-for (let i =0; i < team.length; i++) {
-    let head = document.createElement("div");
-    nose.appendChild(head);
-    head.setAttribute("id", "teamcard"+i);
-    head.innerHTML = ` <img id="img${[i]}" src='${team[i].image}'` +
-    "<br>" +
-    "<p>" +
-    team[i].name +
-    "</p>" +
-    "<p>"
-    +
-    "</p>"
-    +
-    "<div id=moreInfo" +
-    i +
-    ">" +
-    "</div>"
-}
+$(".img-container").on("click",function(){
+    var index = $(this).attr("serri");
+    console.log(index);
+    $(this).find("p").html(jsonData[index].name + "<br>"+jsonData[index].relation + "<br>" + jsonData[index].hobby);
+    
+})
+
+
+// for (let i =0; i < images.length; i++) {
+//     let head = document.createElement("div");
+//     nose.appendChild(head);
+//     // head.setAttribute("id", "teamcard"+i);
+//     .container.innerHTML = ` <img id="img${[i]}" src='${images[i].image}'` +
+//     "<br>" +
+//     "<p>" +
+//     images[i].name +
+//     "</p>" +
+//     "<p>"
+//     +
+//     "</p>"
+//     +
+//     "<div id=moreInfo" +
+//     i +
+//     ">" +
+//     "</div>"
+// }
 
 /* ------------------------------------------------------------------------------------------*/
 
 /*Expand function for teamcards making them bigger and adding additional information  (hobby + age) */
-function expand(x) {
-    document.getElementById("moreInfo" + x).innerHTML = ` <a class="pagelink" href="${team[x].page}"> Page </a>`+
-      `<p>Hobby: ` + team[x].hobby + `</p><p>Age ${team[x].age}`;
-    document.getElementById("teamcard" + x).style.width = "60%";
-  }
-  /* Close Function for clearing the content which comes from the expand function and resizing to 30% */
-  function close(y) {
-    document.getElementById("moreInfo" + y).innerHTML = "";
-    document.getElementById("teamcard" + y).style.width = "35%";
-  }
-  /* creating variable for if statement */
-  let test = 2;
+// function expand(x) {
+//     document.getElementById("moreInfo" + x).innerHTML = ` <a class="pagelink" href="${team[x].page}"> Page </a>`+
+//       `<p>Hobby: ` + team[x].hobby + `</p><p>Age ${team[x].age}`;
+//     document.getElementById("teamcard" + x).style.width = "60%";
+//   }
+//   /* Close Function for clearing the content which comes from the expand function and resizing to 30% */
+//   function close(y) {
+//     document.getElementById("moreInfo" + y).innerHTML = "";
+//     document.getElementById("teamcard" + y).style.width = "35%";
+//   }
+//    creating variable for if statement 
+//   let test = 2;
   
-  /* Loop for adding eventlistener to every div and if/else statement for expand/close indication  */
-  for (let i = 0; i < team.length; i++) {
-    document.getElementById("teamcard" + i).addEventListener(
-      "click",function () {
-        if (test % 2 == 0) {
-          expand(i);
-          test++; /* adds +1 to the test value = uneven*/
-        } else {
-          close(i); /*makes var test even again  */
-          test++;
-        }
-      },
-      false
-    );
-  }
+//   /* Loop for adding eventlistener to every div and if/else statement for expand/close indication  */
+//   for (let i = 0; i < team.length; i++) {
+//     document.getElementById("teamcard" + i).addEventListener(
+//       "click",function () {
+//         if (test % 2 == 0) {
+//           expand(i);
+//           test++; /* adds +1 to the test value = uneven*/
+//         } else {
+//           close(i); /*makes var test even again  */
+//           test++;
+//         }
+//       },
+//       false
+//     );
+//   }
 
-// we loop every relation attribute, and check if its equal to different values. If it is, we add a border to said image, selecting it usind the
-// id that re previously assigned to said image.
-// for(var i=0;i<jsonData.length;i++)
-// {
-//     if(jsonData[i].relation == 'father')
-//     {
-//         $(`#img${i}`).css("border", "blue 2px solid");
-//     } 
-//     else if(jsonData[i].relation == 'grandpa')
-//     {
-//         $(`#img${i}`).css("border", "yellow 2px solid");
-//     } 
-//     else if(jsonData[i].relation == 'dog')
-//     {
-//         $(`#img${i}`).css("border", "red 2px solid");
-//     } 
-//     else if(jsonData[i].relation == 'sister')
-//     {
-//         $(`#img${i}`).css("border", "green 2px solid");
-//     } 
-    
-// }
